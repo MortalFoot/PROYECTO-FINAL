e70eee9ec2fb948694d5969fa8f210ab8db07b4f
alumno Secundaria <al3201@cedi.local>
2018-10-01 08:42:29 -0500

    Agregamos PyE

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HistoriaPage } from '../historia/historia';
 import { DibujoPage } from '../dibujo/dibujo';
+import { PyEPage } from '../py-e/py-e';
 
 @Component({
   selector: 'page-home',
@@ -10,6 +11,7 @@ import { DibujoPage } from '../dibujo/dibujo';
 export class HomePage {
 Historia = HistoriaPage;
 Dibujo = DibujoPage;
+PyE = PyEPage
   constructor(public navCtrl: NavController) {
 
   }
@@ -20,5 +22,9 @@ Dibujo = DibujoPage;
   clickDibujo()
   {
     this.navCtrl.push(this.Dibujo)
+  }
+  clickPyE()
+  {
+    this.navCtrl.push(this.PyE)
   }
 }
